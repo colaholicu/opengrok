6a44859c364d4f71ce7bfce569f13d53c14643ec
Iosif M <colaholicu@users.noreply.github.com>
2022-04-20 22:05:47 +0200

    + added selection search in default project

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,14 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import { config } from 'process';
 import * as vscode from 'vscode';
+import { env } from 'vscode';
+
+export function getConfig() {
+	const url = vscode.workspace.getConfiguration('opengrok').serverURL;
+	const project = vscode.workspace.getConfiguration('opengrok').defaultProjectName;
+	return { url, project };
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -9,9 +17,27 @@ export function activate(context: vscode.ExtensionContext) {
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('opengrok.searchDefaultProject', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from OpenGrok!');
+		const {url, project} = getConfig();
+		if (!url) {
+			vscode.window.showErrorMessage("Server URL is empty.");
+			return;
+		}
+		if (!project) {
+			vscode.window.showErrorMessage("Default Project is not set.");
+			return;
+		}
+
+		const editor = vscode.window.activeTextEditor;
+		if (editor) {
+			const document = editor.document;
+			const selection = editor.selection;
+
+			// Get the word within the selection
+			const selectedText = document.getText(selection);
+			const query = url + "/search?project=" + project + "&q=" + selectedText + "&defs=&refs=&path=&hist=&type=";
+			env.openExternal(vscode.Uri.parse(query));
+		}
+
 	});
 
 	context.subscriptions.push(disposable);
